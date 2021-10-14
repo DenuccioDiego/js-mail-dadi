0d0d8a2bd524604ada6e8b6ed77abc625c3b7d8e
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-14 16:02:39 +0200

    Esercizio DADI completo

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,28 +1,29 @@
-const numeroRandomUser = Math.floor(Math.random() * 6) + 1 //Numero randomico dell'utente
+const numeroRandomUser = Math.floor(Math.random() * 6) + 1 ;     //Numero randomico dell'utente
 
-const numeroRandomComputer = Math.floor(Math.random() * 6) + 1 //Numero randomico del computer
+const numeroRandomComputer = Math.floor(Math.random() * 6) + 1 ; //Numero randomico del computer
 
-const body = document.querySelector("body") //selezione il body
+const body = document.querySelector("body");    //selezione il body
 
 
-body.insertAdjacentHTML('beforeend', `<div>Numero Utente: ${numeroRandomUser}</div>`) //creo nel body un div che conterrà il numero randomico dell'utente
-body.insertAdjacentHTML('beforeend', `<div>Numero Computer: ${numeroRandomComputer}</div>`)  //creo nel body un div che conterrà il numero randomico del computer
+body.insertAdjacentHTML('beforeend', `<div>Numero Utente: ${numeroRandomUser}</div>`);        //creo nel body un div che conterrà il numero randomico dell'utente
+body.insertAdjacentHTML('beforeend', `<div>Numero Computer: ${numeroRandomComputer}</div>`);  //creo nel body un div che conterrà il numero randomico del computer
 
-//dichiaro chi ha vinto tra determinando il valore più alto 
 
-if(numeroRandomUser > numeroRandomComputer ) {
-     body.insertAdjacentElement('beforeend', `<div>Complimenti hai vinto tu</div>`) 
-     console.log("vinto")
+//dichiaro il risultato della sfida tramite le seguenti condizioni 
+
+if(numeroRandomUser > numeroRandomComputer) {   // si applica questa condizione nel caso il n-User è maggiore n-Pc 
+     body.insertAdjacentHTML('beforeend', `<div>Complimenti hai vinto tu</div>`) ; //creazione del div 
+     //console.log("vinto")
 }
 
-if (numeroRandomUser < numeroRandomComputer) {
-     // body.insertAdjacentElement('beforeend', `<div>Mi dispiace ha vinto il PC</div>` )
-     console.log("perso")
+if (numeroRandomUser < numeroRandomComputer) {    //si applica questa condizione nel caso il n-User è minore n-Pc
+     body.insertAdjacentHTML('beforeend', `<div>Mi dispiace ha vinto il Pc</div>`); //creazione del div 
+     //console.log("perso")
 }
 
-else if (numeroRandomUser == numeroRandomComputer) {
-     // body.insertAdjacentElement('beforeend', `<div>Pareggio</div>` )
-     console.log("pareggio")
+else if (numeroRandomUser == numeroRandomComputer) { //si applica questa condizione nel caso il n-User è uguale n-Pc
+     body.insertAdjacentHTML('beforeend', `<div>Pareggio</div>`);                   //creazione del div 
+     //console.log("pareggio")
 }
 
 
